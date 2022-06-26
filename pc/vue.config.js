4ceec476eda75a96ad124d5dd493e8e94a7fcf3a
colin <13067565776@163.com>
2022-06-26 13:59:59 +0800

    1

--- pc/vue.config.js
+++ pc/vue.config.js
@@ -30,8 +30,8 @@ module.exports = {
     }, // 错误在页面弹出、警告不在页面弹出
     proxy: {
       '/api': {
-        // target: 'http://47.118.76.70:8080/', // 代`理地址，这里设置的地址会代替axios中设置的baseURL
-        target: 'http://192.168.0.20:8080', // cl
+        target: 'http://47.118.76.70:8080/', // 代`理地址，这里设置的地址会代替axios中设置的baseURL
+        // target: 'http://192.168.0.20:8080', // cl
         // target: 'http://192.168.0.112:8080', // jt
         // target: 'http://192.168.0.136:8080', // lf
         // target: 'http://192.168.0.21:8080', // zs
